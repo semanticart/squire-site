0be989577898271280b85e4f811e92046a55c48f
Jeffrey Chupp <jeff@semanticart.com>
2024-03-16 09:51:51 -0400

    Syntax example and update squire pkg

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/semanticart/squire-site
 
 go 1.22.1
 
-require github.com/semanticart/squire v0.0.0-20240315124203-39ad5efdcaa3
+require github.com/semanticart/squire v0.0.0-20240322201612-af05a8174a9d
--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
-	"github.com/semanticart/squire"
+	"github.com/semanticart/squire/pkg"
 )
 
 //go:embed static/*
@@ -19,7 +21,7 @@ var t = template.Must(template.ParseFS(staticFiles, "static/*"))
 
 const maxMemorySize = 10 << 20 // 10MB in bytes
 
-func executeTemplateToBytes(templateName string, data interface{}) []byte {
+func executeTemplateToBytes(templateName string, data Page) []byte {
 	var buf bytes.Buffer
 	err := t.ExecuteTemplate(&buf, templateName, data)
 	if err != nil {
@@ -32,6 +34,12 @@ type Page struct {
 	Title      string
 	Story      squire.Story
 	ShowErrors bool
+	Errors     []Error
+}
+
+type Error struct {
+	Line    int
+	Message string
 }
 
 var empty = map[string]string{}
@@ -45,37 +53,59 @@ func main() {
 	index := executeTemplateToBytes("index.html.tmpl", Page{Title: "Squire", Story: squire.Story{}})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(index))
+		switch r.Method {
+		case "GET":
+			w.Write([]byte(index))
+		case "POST":
+			if err := r.ParseMultipartForm(maxMemorySize); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			file, _, err := r.FormFile("fileUpload")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer file.Close()
+
+			content, err := io.ReadAll(file)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			story, err := squire.ParseStory(string(content))
+
+			errors := make([]Error, 0)
+
+			if err != nil {
+				for _, e := range strings.Split(err.Error(), "\n") {
+					parts := strings.SplitN(e, ":", 2)
+					if len(parts) == 2 {
+						lineNumber, err := strconv.Atoi(parts[0])
+						if err == nil {
+							message := parts[1]
+							errors = append(errors, Error{Line: lineNumber, Message: message})
+						} else {
+							errors = append(errors, Error{Line: 0, Message: e})
+						}
+					} else {
+						errors = append(errors, Error{Line: 0, Message: e})
+					}
+				}
+			}
+
+			error := executeTemplateToBytes("index.html.tmpl", Page{Title: "Squire", Story: story, Errors: errors, ShowErrors: true})
+			w.Write(error)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
 	})
 
 	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(bookStyleSheet)
-	})
-
-	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
-		if err := r.ParseMultipartForm(maxMemorySize); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		file, _, err := r.FormFile("fileUpload")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer file.Close()
-
-		content, err := io.ReadAll(file)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		story := squire.ParseStory(string(content))
-
-		error := executeTemplateToBytes("index.html.tmpl", Page{Title: "Squire", Story: story, ShowErrors: true})
-		w.Write(error)
 	})
 
 	port := os.Getenv("PORT")
